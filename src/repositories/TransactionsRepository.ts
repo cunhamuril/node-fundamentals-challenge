8918f5bc6a4bb864bdb84de7a8c02f10d672df37
Murilo Cunha <murilo.sant@hotmail.com>
2020-08-25 13:15:45 -0300

    Challenge finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomes = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .map(transaction => transaction.value);
+
+    const outcomes = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .map(transaction => transaction.value);
+
+    const income = incomes.reduce((acc, item) => acc + item, 0);
+    const outcome = outcomes.reduce((acc, item) => acc + item, 0);
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    const { total } = this.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw Error('Can not outcome transaction without a valid balance');
+    }
+
+    return transaction;
   }
 }
 
